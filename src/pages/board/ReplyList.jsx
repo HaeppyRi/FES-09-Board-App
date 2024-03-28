20bb04843044b550dab769cfc230c48bb820c62e
HaeriKim <kimkiimm@naver.com>
2024-03-28 14:06:56 +0900

    수정

--- src/pages/board/ReplyList.jsx
+++ src/pages/board/ReplyList.jsx
@@ -2,7 +2,7 @@ import useCustomAxios from "@hooks/useCustomAxios.mjs";
 import ReplyItem from "@pages/board/ReplyItem";
 import ReplyNew from "@pages/board/ReplyNew";
 import { useParams } from "react-router-dom";
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 
 // import { useEffect, useState } from "react";
@@ -25,11 +25,19 @@ function ReplyList(){
   // }, []);
 
 
-  const { data } = useQuery({
+  const { data } = useInfiniteQuery({
     queryKey: ['posts', _id, 'replies'],
-    queryFn: () => axios.get(`/posts/${ _id }/replies`, { params: { sort: JSON.stringify({ _id: -1 }) } }),
+    queryFn: ({ pageParam=1 }) => axios.get(`/posts/${ _id }/replies?delay=3000`, { params: { page: pageParam, limit: import.meta.env.VITE_REPLY, sort: JSON.stringify({ _id: -1 }) } }),
     select: response => response.data,
     // refetchInterval: 1000
+
+    // 마지막 페이지와 함께 전체 페이지 목록을 받아서 queryFn에 전달할 pageParam 값을 리턴하도록 구현한다.
+    // false를 리턴하면 더 이상 queryFn이 호출되지 않고 무한 스크롤 종료
+    getNextPageParam: (lastPage, allPage) => {
+      console.log(lastPage, allPage);
+      // const totalPage = lastPage.data
+      return 2;
+    }
   });
 
   const list = data?.item.map(item => <ReplyItem key={ item._id } item={ item } />);
